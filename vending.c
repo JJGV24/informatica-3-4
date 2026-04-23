5b86b84aa1605f8620379b2fb21b15f39bdd2c4c
Josue Garcia <josuegarciav24@gmail.com>
2026-04-23 15:30:44 +0000

    finish vending machine

---
+++ vending.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+//variables for snck 1 (tostiviches)
+    float price1 = 119.99;
+    int stock1 = 15;
+    char selcode1 = 'A';
+//variables for snack 2 (papitas)
+   float price2 = 24.99;
+    int stock2 = 30;
+    char selcode2 = 'B';
+    //variables for snack 3 (Shake)
+   float price3 = 79.99;
+    int stock3 = 10;
+    char selcode3 = 'C';
+
+ //   - `%d` for `int`
+//- `%.2f` for `float`
+//- `%lf` for `double`
+//- `%c` for `char`
+
+    printf("Welcome the the Snack Shop\n");
+    printf("---------------------------\n");
+    printf("Item        Price     Quantity   Code\n");
+    printf("tostiviches %.2f        %d       %c\n", price1, stock1, selcode1);
+    printf("papitas     %.2f         %d       %c\n", price2, stock2, selcode2);
+    printf("Shake       %.2f         %d       %c\n", price3, stock3, selcode3);
+
+}
