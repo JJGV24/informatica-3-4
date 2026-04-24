4cadcd1661c642387e277aafca263d5b8ae8a1eb
Josue Garcia <josuegarciav24@gmail.com>
2026-04-24 17:12:12 +0000

    comitfolder

--- weekly checkpoint 1/calculator.c
+++ weekly checkpoint 1/calculator.c
@@ -4,6 +4,7 @@ int main(void)
 {
     int x;
     int y;
+
     printf("height =");
     scanf("%d",&x);
 
@@ -12,6 +13,7 @@ int main(void)
 
     int A = x*y;
     int P = 2*x+2*y;
+    
     printf("area=%d\n", A);
     printf("perimeter=%d\n", P);
 }
