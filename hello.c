b2b34cf91e24ebacee305b3d0b38c9860ebd949a
Josue Garcia <josuegarciav24@gmail.com>
2026-04-24 14:50:06 +0000

    asking machine

--- hello.c
+++ hello.c
@@ -2,6 +2,16 @@
 
 int main(void)
 {
-    printf("hello Josue! \n");
+// ask for name and salute
+    char user_name[50];
+ printf("what is your name? ");
+scanf("%s", user_name);
+printf("Hi %s\n", user_name);
+
+//ask for FavColor
+ char FavColor[20];
+ printf("what is your favorite color? ");
+scanf("%s", FavColor);
+printf("Your favorite color is %s\n", FavColor);
 }
 
