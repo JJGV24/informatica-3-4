afb1f553a7d64dc2e03b9ae4065682696f0ca7d6
Josue Garcia <josuegarciav24@gmail.com>
2026-04-29 17:26:51 +0000

    me va a poner cero profe

---
+++ test/Receipt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+//variables
+    char item[15];
+    float price;
+    int quantity;
+
+printf("item? \n= ");
+scanf("%d", &item);
+
+printf("price? \n= ");
+scanf("%f", &price);
+
+printf("quantity? \n= ");
+scanf("%d", &quantity);
+float total = price*quantity;
+
+    printf("***********************\n");
+    printf("Abarrotes Cesar\n");
+    printf("***********************\n");
+    printf("Item       Quantity   Price\n");
+    printf("%c          %d          %f\n",item ,quantity ,price);
+}
