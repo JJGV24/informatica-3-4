cf3d68b8b0fdf70338eb2d0650ef1480335398ec
Josue Garcia <josuegarciav24@gmail.com>
2026-05-07 19:29:04 +0000

    not working the minute thing

--- week 2/minute.c
+++ week 2/minute.c
@@ -12,15 +12,20 @@ int main(void)
     printf("How long is your movie in hr? \n= ");
     scanf("%d:%d",&movruntimeh, &movruntimem);
 
-    int min_t = movruntimeh*60+movruntimem;
+    int min_t = (movruntimeh*60)+movruntimem;
     printf("Movie running time (in minutes): %d\n", min_t);
 
     printf("Starting time: \n= ");
     scanf("%d:%d", &starttimeh, &startimem);
 
-    endth = min_t/60+starttimeh;
-    endtm = min_t%60+startimem;
+    endth = (min_t/60)+starttimeh;
+    endtm = min_t%60;
 
-    printf("The movie will end at %d:%d \n", endth, endtm);
+    if (endtm >= 60){
+            endth = endth + 1; //update value of endhour variable
+            endtm = endtm - 60;
+    }
+
+    printf("The movie will end at %d:%02d \n", endth, endtm);
 
 }
